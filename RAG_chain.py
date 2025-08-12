3920c06e167be992a90168ec8948e6b9e2bea70f
Kalash Srivastava <kalashsrivastava0189@gmail.com>
2025-08-12 13:38:56 +0530

    RAG_chain.py

--- RAG_chain.py
+++ RAG_chain.py
@@ -14,13 +14,13 @@ def format_docs(docs):
 
 def rag_chain_pipeline():
     # First we will laod the document which we like to load 
-    loader  = PyPDFLoader(file_path=r"C:\Users\Kalash Srivastava\RAG\Research_Paper_On_Artificial_Intelligence_And_Its.pdf")
+    loader  = PyPDFLoader(file_path="your file path")
     docs = loader.load()
 
     # Now we will split the data into smaller chunks and store in vector database (Chroma)
     text_splitter  = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap=200)
     split = text_splitter.split_documents(docs)
-    vectore_db = Chroma.from_documents(documents=split,embedding=CohereEmbeddings(model="embed-english-v3.0",cohere_api_key="dAnko2bn2OA0app21L6Ahxg9tT58l21sTeE4gH4l"))
+    vectore_db = Chroma.from_documents(documents=split,embedding=CohereEmbeddings(model="embed-english-v3.0",cohere_api_key="Your api key"))
 
     # we want the best output so we use as _reriever() to find the best match
     retriver = vectore_db.as_retriever()
@@ -30,7 +30,7 @@ def rag_chain_pipeline():
     prompt = hub.pull("rlm/rag-prompt")
 
     # Now we want to intialize the LLM chat model we will use like for Cohere 
-    llm = ChatCohere(cohere_api_key="dAnko2bn2OA0app21L6Ahxg9tT58l21sTeE4gH4l")
+    llm = ChatCohere(cohere_api_key="your api key")
 
 
     rag_chain = ({'context':retriver|format_docs,'question':RunnablePassthrough()}
@@ -46,6 +46,7 @@ rag_chain = rag_chain_pipeline()
 
 def ask_query(question):
     return rag_chain.invoke(question)
+
 
 
 
